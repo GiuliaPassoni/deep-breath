7e8a8aeadb87cefbc7bd0d1a7afc707aa6afafe8
Giulia Passoni <giulia@colour-profiling.com>
2023-06-11 15:34:15 +0100

    feat: added Boolean param, fixed variable scope for time and radii vars.

--- src/utils/drawCircle.ts
+++ src/utils/drawCircle.ts
@@ -1,16 +1,19 @@
 import React from "react";
-import {select} from "d3";
+import {min, select} from "d3";
 
-export function drawCircle(svgRef: React.MutableRefObject<SVGSVGElement | null>, circleId: string){
+export function drawCircle(svgRef: React.MutableRefObject<SVGSVGElement | null>, circleId: string, increase: Boolean){
     const svg = svgRef.current
-    let minRadius: number = 2, maxRadius: number = 200, radius: number = minRadius
-    let time:number = 0,
-        timeIncrease: number = time*2, timeDecrease: number = time*.5
+    let time:number = 0
+        // timeIncrease: number = time*2, timeDecrease: number = time*.5
 
     let myTimerCounter: number=0
 
     const myInterval: NodeJS.Timer = setInterval(()=>{
-    // setInterval(()=>{
+        let timeIncrease: number = time*2, timeDecrease: number = time*.5
+        console.log('timeIncrease', timeIncrease, 'timeDecrease', timeDecrease)
+        let minRadius: number = 2, maxRadius: number = 200,
+            radius: number = increase ? minRadius : maxRadius
+
         myTimerCounter += 1
         if(myTimerCounter === 250){
             clearInterval(myInterval)
@@ -19,32 +22,13 @@ export function drawCircle(svgRef: React.MutableRefObject<SVGSVGElement | null>,
                 .attr('id', `${circleId}`)
                 .attr('cx', '50%')
                 .attr('cy', '50%')
-                // .attr('r', radius > maxRadius? radius*timeDecrease : radius*timeIncrease )
-                .attr('r', radius*time )
+                .attr('stroke','black')
+                // .attr('r', 200-2*time)
+                .attr('r', increase ? radius*time : radius-time)
+                // .attr('r', radius*time )
                 .style('fill', '#a6a6a6');
         }
-        time+=.5
+        time+=.25
         console.log('time', time)
     }, time)
-
-    // clearInterval(myInterval)
 }
-
-// export function growCircle(circleRef: React.MutableRefObject<SVGSVGElement | null>, ){
-//     const firstCircleRef = circleRef.current
-//
-//     let initialRadius = 20,
-//         lastRadius = 50
-//
-//     setTimeout(()=>{
-//         select(firstCircleRef).append('rect')
-//             .attr('x', 10)
-//             .attr('y', 120)
-//             .attr('width', 600)
-//             .attr('height', 40)
-//             .attr('stroke', 'black')
-//             .attr('fill', '#69a3b2');
-//
-//         console.log('timeout')
-//     }, 1000)
-// }
